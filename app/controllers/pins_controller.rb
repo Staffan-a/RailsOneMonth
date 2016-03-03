619dfea9712d88907584fc50d3d138918867f796
Staffan <staffan.a@gmail.com>
2016-03-03 21:48:23 +0100

    Image uppload functionallity added with paperclip gem

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -28,7 +28,6 @@ class PinsController < ApplicationController
   end
 
   def update
-
     if @pin.update(pin_params)
       redirect_to @pin, notice: 'Pin was successfully updated.'
     else
@@ -54,6 +53,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
